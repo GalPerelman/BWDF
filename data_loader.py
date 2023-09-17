cc56b7520f3beb6546d4194effd6526e6f41e4b7
GalPerelman <peligur@gmail.com>
2023-09-17 16:57:49 +0300

    reorganize data loading and preprocess

--- data_loader.py
+++ data_loader.py
@@ -14,28 +14,6 @@ class Loader:
     def __init__(self):
         self.inflow = self.load_excel(os.path.join(constants.RESOURCES, "InflowData_1.xlsx"))
         self.weather = self.load_excel(os.path.join(constants.RESOURCES, "WeatherData_1.xlsx"))
-        self.data = pd.merge(self.inflow, self.weather, left_index=True, right_index=True, how="outer")
-        self.datetime_categorize()
-
-    def datetime_categorize(self):
-        """
-        Function to add categories based on the datetime index
-        Month, weekday, hour, special dates
-        """
-        self.data['month'] = self.data.index.month
-        self.data['day'] = self.data.index.day
-        self.data['hour'] = self.data.index.hour
-        self.data['weekday'] = self.data.index.day_name()
-        self.data['weekday_int'] = (self.data.index.weekday + 1) % 7 + 1
-        self.data['week_num'] = self.data.index.strftime('%U').astype(int) + 1
-
-        self.data.index = self.data.index.tz_localize('UTC').tz_convert(constants.TZ)
-
-        def is_dst(dt):
-            return dt.dst() != pd.Timedelta(0)
-
-        self.data['is_dst'] = self.data.index.map(is_dst)
-        self.data['is_special'] = self.data.index.normalize().isin(constants.SPECIAL_DATES)
 
     @staticmethod
     def load_excel(file_path: str):
@@ -47,4 +25,5 @@ class Loader:
         """
         df = pd.read_excel(file_path, index_col=0)
         df.index = pd.to_datetime(df.index, format="%d/%m/%Y %H:%M")
+        df.index = df.index.tz_localize('UTC').tz_convert(constants.TZ)
         return df
--- preprocess.py
+++ preprocess.py
@@ -1,15 +1,42 @@
 import pandas as pd
 from sklearn.impute import KNNImputer
 
+import constants
+
 
 class Preprocess:
-    def __init__(self, data: pd.DataFrame):
-        self.data = data
+    def __init__(self, inflow: pd.DataFrame, weather: pd.DataFrame, n_neighbors: int):
+        self.inflow = inflow
+        self.weather = weather
+        self.n_neighbors = n_neighbors
 
-    def data_completion(self, data: pd.DataFrame, n_neighbors: int):
-        knn_impute = KNNImputer(n_neighbors=n_neighbors)
+        self.inflow = self.data_completion(self.inflow)
+        self.weather = self.data_completion(self.weather)
+        self.data = pd.merge(self.inflow, self.weather, left_index=True, right_index=True, how="left")
+        self.datetime_categorize()
+
+    def data_completion(self, data):
+        knn_impute = KNNImputer(n_neighbors=self.n_neighbors)
         data_imputed = knn_impute.fit_transform(data)
-        data = pd.DataFrame(data_imputed, columns=data.columns, index=self.data.index)
-        return data
+        data_imputed = pd.DataFrame(data_imputed, columns=data.columns, index=data.index)
+        return data_imputed
+
+    def datetime_categorize(self):
+        """
+        Function to add categories based on the datetime index
+        Month, weekday, hour, special dates
+        """
+        self.data['month'] = self.data.index.month
+        self.data['day'] = self.data.index.day
+        self.data['hour'] = self.data.index.hour
+        # self.data['weekday'] = self.data.index.day_name()
+        self.data['weekday_int'] = (self.data.index.weekday + 1) % 7 + 1
+        self.data['week_num'] = self.data.index.strftime('%U').astype(int) + 1
+
+        def is_dst(dt):
+            return dt.dst() != pd.Timedelta(0)
+
+        self.data['is_dst'] = self.data.index.map(is_dst)
+        self.data['is_special'] = self.data.index.normalize().isin(constants.SPECIAL_DATES)
 
 
