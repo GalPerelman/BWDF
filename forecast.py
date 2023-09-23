869dad75d8af6c8dc0db16bb4b10bdcfe4ec3995
GalPerelman <peligur@gmail.com>
2023-09-23 10:16:46 +0300

    improve params search work flow

--- forecast.py
+++ forecast.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import xgboost as xgb
 from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor, HistGradientBoostingRegressor
 from sklearn.gaussian_process import GaussianProcessRegressor
@@ -11,26 +12,37 @@ warnings.filterwarnings(action="ignore")
 
 
 class Forecast:
-    def __init__(self, data, y_label, models, len_test=24):
+    def __init__(self, data, y_label, models, len_train, len_test, name):
         self.data = data
         self.y_label = y_label
         self.models = models
+        self.len_train = len_train
         self.len_test = len_test
+        self.name = name
+
         self.x_train, self.y_train, self.x_test, self.y_test = self.split_data()
 
     def split_data(self):
         x_columns = list(self.data.columns)
         x_columns.remove(self.y_label)
 
-        x_train = self.data.iloc[:-self.len_test].loc[:, x_columns]
-        y_train = self.data.iloc[:-self.len_test].loc[:, self.y_label]
+        x_train = self.data.iloc[-(self.len_train + self.len_test): -self.len_test].loc[:, x_columns]
+        y_train = self.data.iloc[-(self.len_train + self.len_test): -self.len_test].loc[:, self.y_label]
 
         x_test = self.data.iloc[-self.len_test:].loc[:, x_columns]
         y_test = self.data.iloc[-self.len_test:].loc[:, self.y_label]
         return x_train, y_train, x_test, y_test
 
     def xgb(self):
-        reg = xgb.XGBRegressor(n_estimators=1000)
+        reg = xgb.XGBRegressor(n_estimators=500,
+                               bootstrap=False,
+                               max_depth=7,
+                               min_samples_leaf=2,
+                               min_samples_split=3,
+                               colsample_bytree=0.7,
+                               learning_rate=0.2,
+                               reg_alpha=0.5
+                               )
         reg.fit(self.x_train, self.y_train, verbose=True)
 
         pred = pd.DataFrame(reg.predict(self.x_test), index=self.x_test.index)
@@ -38,9 +50,5 @@ class Forecast:
 
     def search(self):
         for model_name, model_info in self.models.items():
-            searcher = Searcher(model_name, model_info, self.x_train, self.y_train)
+            searcher = Searcher(model_name, model_info, self.x_train, self.y_train, self.y_label, self.name)
             searcher.grid_search()
-            searcher.ga_search()
-            searcher.export_results()
-
-
--- hyperparam_search.py
+++ hyperparam_search.py
@@ -1,4 +1,6 @@
+import timeit
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import GridSearchCV, TimeSeriesSplit
 from sklearn_genetic import GASearchCV
 from sklearn_genetic.space import Continuous, Categorical, Integer
@@ -10,13 +12,15 @@ xgb.set_config(verbosity=0)
 
 
 class Searcher:
-    def __init__(self, model_name, model_info, x_train, y_train, n_splits=3):
+    def __init__(self, model_name, model_info, x_train, y_train, y_label, name, n_splits=3):
         self.model_name = model_name
         self.model = model_info['model']
         self.params = model_info['params']
         self.ga_params = model_info['ga_params']
         self.x_train = x_train
         self.y_train = y_train
+        self.y_label = y_label
+        self.name = name
         self.n_splits = n_splits
 
         self.tscv = TimeSeriesSplit(n_splits=self.n_splits)
@@ -24,11 +28,12 @@ class Searcher:
 
     def grid_search(self):
         gs_model = GridSearchCV(estimator=self.model, param_grid=self.params, cv=self.tscv, verbose=3,
-                                scoring='neg_mean_absolute_error')
+                                scoring='neg_mean_absolute_error', n_jobs=-1)
 
         gs_model.fit(self.x_train, self.y_train)
         print(gs_model.best_params_, gs_model.best_score_)
-        self.record(self.model_name, gs_model.best_params_, "neg_mean_absolute_error", gs_model.best_score_)
+        results = pd.DataFrame(gs_model.cv_results_)
+        results.to_csv(f'{self.name}_{self.y_label[:-6]}_{self.model_name}.csv', index=False)
 
     def ga_search(self):
         ga_model = GASearchCV(estimator=self.model, param_grid=self.ga_params, scoring="neg_mean_absolute_error",
@@ -36,7 +41,8 @@ class Searcher:
 
         ga_model.fit(self.x_train, self.y_train)
         print(self.model_name, ga_model.best_params_, ga_model.best_score_)
-        self.record(self.model_name, ga_model.best_params_, "neg_mean_absolute_error", ga_model.best_score_)
+        results = pd.DataFrame(ga_model.cv_results_)
+        results.to_csv(f'{self.y_label[:-6]}_{self.model_name}.csv', index=False)
 
     def record(self, model_name, best_params, score_metric, score_value):
         df = pd.DataFrame({'model_name': model_name,
@@ -46,5 +52,27 @@ class Searcher:
                           index=[len(self.results)])
         self.results = pd.concat([self.results, df])
 
-    def export_results(self):
-        self.results.to_csv('hp.csv')
+    def export_results(self, export_file_name):
+        self.results.to_csv(f'{export_file_name}_params.csv')
+
+    def estimate_gridsearch_time(self):
+        times = []
+        for _ in range(5):
+            start = timeit.default_timer()
+            self.model.fit(self.x_train, self.y_train)
+            self.model.score(self.x_train, self.y_train)
+            times.append(timeit.default_timer() - start)
+            print(times)
+
+        single_train_time = np.array(times).mean()  # seconds
+        print(single_train_time)
+
+        combos = 1
+        for vals in self.params.values():
+            combos *= len(vals)
+
+        num_models = combos * self.n_splits
+        seconds = num_models * single_train_time
+        minutes = seconds / 60
+        hours = minutes / 60
+        print(f"Estimated search time {hours:.1f} hours")
