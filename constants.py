27dc544ff59bc56bd6e9f9757346bb4616c942a3
GalPerelman <peligur@gmail.com>
2023-09-28 18:09:47 +0300

    add time to test periods to avoid wrong train test split

--- constants.py
+++ constants.py
@@ -52,10 +52,17 @@ SPECIAL_DATES = [
 ]
 
 TEST_TIMES = {
-    'w1': {'start': datetime.datetime(2022, 7, 25, tzinfo=TZ), 'end': datetime.datetime(2022, 8, 1, tzinfo=TZ)},
-    'w2': {'start': datetime.datetime(2022, 10, 31, tzinfo=TZ), 'end': datetime.datetime(2022, 11, 6, tzinfo=TZ)},
-    'w3': {'start': datetime.datetime(2023, 1, 16, tzinfo=TZ), 'end': datetime.datetime(2023, 1, 23, tzinfo=TZ)},
-    'w4': {'start': datetime.datetime(2023, 3, 6, tzinfo=TZ), 'end': datetime.datetime(2023, 3, 13, tzinfo=TZ)}
+    'w1': {'start': datetime.datetime(2022, 7, 25, 0, 0,tzinfo=TZ),
+           'end': datetime.datetime(2022, 8, 1, 0, 0, tzinfo=TZ)},
+
+    'w2': {'start': datetime.datetime(2022, 10, 31, 0, 0, tzinfo=TZ),
+           'end': datetime.datetime(2022, 11, 6, 0, 0, tzinfo=TZ)},
+
+    'w3': {'start': datetime.datetime(2023, 1, 16, 0, 0, tzinfo=TZ),
+           'end': datetime.datetime(2023, 1, 23, 0, 0, tzinfo=TZ)},
+
+    'w4': {'start': datetime.datetime(2023, 3, 6, 0, 0, tzinfo=TZ),
+           'end': datetime.datetime(2023, 3, 13, 0, 0, tzinfo=TZ)}
 }
 
 DATES_TO_TEST_EXTREME_RAINFALL = {
