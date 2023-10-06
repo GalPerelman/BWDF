4009930ea0177ce504cda7c13ef8b01b38bf38ad
GalPerelman <peligur@gmail.com>
2023-10-06 10:20:59 +0300

    add function to read json

--- utils.py
+++ utils.py
@@ -1,7 +1,13 @@
 import os
+import json
 
 
 def validate_dir_path(path):
     if not os.path.exists(path):
         os.makedirs(path)
     return path
+
+
+def read_json(file):
+    with open(file) as f:
+        return json.load(f)
