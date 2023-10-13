81a11eea3e8bfc181ac4019feb3f8bb19970cbff
GalPerelman <peligur@gmail.com>
2023-10-13 14:02:37 +0300

    rename file with parameters grids

--- params_grids.py
+++ params_grids.py
@@ -5,8 +5,9 @@ from sklearn.svm import SVR
 import xgboost as xgb
 from sklearn_genetic.space import Continuous, Categorical, Integer
 
-xgb.set_config(verbosity=0)
+from prophet_model import ProphetForecaster
 
+xgb.set_config(verbosity=0)
 
 models = {
     'rf':
@@ -38,21 +39,35 @@ models = {
             'model': xgb.XGBRegressor(verbosity=0, silent=True),
             'params':
                 {
-                 'bootstrap': [True, False],
-                 "max_depth": [4, 5, 6, 7, 8],
-                 "learning_rate": [0.01, 0.05, 0.1, 0.2],
-                 "n_estimators": [25, 50, 100, 200, 500],
-                 "reg_alpha": [0, 0.1, 0.5],
-                 "min_sample_leaf": [2, 3, None],
-                 "min_sample_split": [2, 3, None]
-                 },
+                    'bootstrap': [True, False],
+                    "max_depth": [4, 5, 6, 7, 8],
+                    "learning_rate": [0.01, 0.05, 0.1, 0.2],
+                    "n_estimators": [25, 50, 100, 200, 500],
+                    "reg_alpha": [0, 0.1, 0.5],
+                    "min_sample_leaf": [2, 3, None],
+                    "min_sample_split": [2, 3, None]
+                },
 
             'ga_params':
                 {
-                 'bootstrap': Categorical([True, False]),
-                 'max_depth': Integer(10, 20),
-                 'learning_rate': Continuous(0.01, 0.4),
-                 'n_estimators': Integer(20, 50)
-                 },
+                    'bootstrap': Categorical([True, False]),
+                    'max_depth': Integer(10, 20),
+                    'learning_rate': Continuous(0.01, 0.4),
+                    'n_estimators': Integer(20, 50)
+                },
+        },
+
+    'prophet':
+        {
+            'model': ProphetForecaster(),
+            'params':
+                {
+                    'seasonality_mode': ['additive', 'multiplicative'],
+                    # 'daily_seasonality': [True, False],
+                    # 'weekly_seasonality': [True, False],
+                    # 'seasonality_prior_scale': [0.01, 0.1, 1.0, 10.0, 20.0],
+                    # 'holidays_prior_scale': [0.01, 0.1, 1.0, 10.0],
+                    'changepoint_prior_scale': [0.001, 0.01]#, 0.1, 0.5],
+                }
         }
 }
