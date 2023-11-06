7958febf9b65ed53c34c9d97685264b225579dc9
GalPerelman <peligur@gmail.com>
2023-11-06 12:50:32 +0200

    generalize lstm to match sklearn structure of fit and predict methods
    clear all init inputs that are not model parameters
    preprocess is done outside the lstm class
    model build is done within fit
    save last 'look_back' periods of train to use in predict

--- lstm_model.py
+++ lstm_model.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np
 import datetime
 import matplotlib.pyplot as plt
+from sklearn.model_selection import GridSearchCV, TimeSeriesSplit
+from sklearn.metrics import make_scorer, mean_absolute_error
+from sklearn.base import BaseEstimator, RegressorMixin
 
 from keras.models import Sequential, load_model
 from keras.layers import Dense, Activation, Dropout, LSTM
@@ -16,73 +19,51 @@ from preprocess import Preprocess
 from data_loader import Loader
 
 
-class LSTMForecaster:
-    def __init__(self, data, start_train, start_valid, start_test, end_test, y_label, look_back, standard_cols):
-        self.data = data
-        self.start_train = start_train
-        self.start_valid = start_valid
-        self.start_test = start_test
-        self.end_test = end_test
-        self.y_label = y_label
+class LSTMForecaster(BaseEstimator, RegressorMixin):
+    def __init__(self, look_back=24, epochs=10, batch_size=24, units=100, dropout=0.2):
         self.look_back = look_back
-        self.standard_cols = standard_cols + [self.y_label]
-
-        self.train_data = None
-        self.valid_data = None
-        self.scalers = None
-
-        self.x_train = None
-        self.y_train = None
-        self.x_valid = None
-        self.y_valid = None
+        self.epochs = epochs
+        self.batch_size = batch_size
+        self.units = units
+        self.dropout = dropout
 
         self.model = None
 
-        self.preprocess()
-        self.train_gen, self.valid_gen = self.build_supervised()
-        self.build_model()
-
-    def preprocess(self):
-        self.data = utils.drop_other_dmas(self.data, self.y_label)
-
-        train = self.data.loc[(self.data.index >= self.start_train) & (self.data.index < self.start_valid)]
-        valid = self.data.loc[(self.data.index >= self.start_valid) & (self.data.index < self.start_test)]
-
-        self.train_data, self.scalers = Preprocess.fit_transform(train, columns=self.standard_cols, method='standard')
-        self.valid_data = Preprocess.transform(valid, columns=self.standard_cols, scalers=self.scalers)
-
-        self.x_train = train.loc[:, [col for col in train.columns if col != self.y_label]]
-        self.y_train = train.loc[:, self.y_label]
-        self.x_valid = valid.loc[:, [col for col in valid.columns if col != self.y_label]]
-        self.y_valid = valid.loc[:, self.y_label]
-
-    def build_supervised(self):
-        train_generator = TimeseriesGenerator(self.x_train, self.y_train, length=self.look_back, batch_size=12)
-        valid_generator = TimeseriesGenerator(self.x_valid, self.y_valid, length=self.look_back, batch_size=12)
-        return train_generator, valid_generator
-
-    def build_model(self):
+    def build_model(self, n_features):
         self.model = Sequential()
-        self.model.add(LSTM(100, input_shape=(self.look_back, self.x_train.shape[1]), return_sequences=True))
-        self.model.add(LSTM(100, return_sequences=False))
-        self.model.add(Dropout(0.2))
+        self.model.add(LSTM(units=self.units, input_shape=(self.look_back, n_features), return_sequences=True))
+        self.model.add(LSTM(units=self.units, return_sequences=False))
+        self.model.add(Dropout(self.dropout))
         self.model.add(Dense(1))
         self.model.compile(optimizer='adam', loss='mse')
 
-    def fit(self):
+    def fit(self, x, y):
+        self.build_model(n_features=x.shape[1])
+        train_size = int(len(x) * 0.8)
+
+        self.train_tail = x.iloc[-self.look_back:]
+        x_train, x_valid = x.iloc[:train_size].values, x.iloc[train_size:].values
+        y_train, y_valid = y.iloc[:train_size].values, y.iloc[train_size:].values
+
+        train_gen = TimeseriesGenerator(x_train, y_train, length=self.look_back, batch_size=self.batch_size)
+        valid_gen = TimeseriesGenerator(x_valid, y_valid, length=self.look_back, batch_size=self.batch_size)
         early_stopping_monitor = EarlyStopping(monitor='val_loss', min_delta=0, patience=0, verbose=0, mode='auto',
                                                baseline=None, restore_best_weights=True)
-        history = self.model.fit(self.train_gen, epochs=12, batch_size=48, validation_data=self.valid_gen,
+        history = self.model.fit(train_gen, epochs=self.epochs, batch_size=self.batch_size, validation_data=valid_gen,
                                  callbacks=[early_stopping_monitor])
+
         return history
 
-    def vaildation_predict(self):
-        pred = self.model.predict(self.valid_gen)
-        pred = self.scalers[self.y_label].inverse_transform(pred)
-        true_values = self.scalers[self.y_label].inverse_transform(self.y_valid.values.reshape(-1, 1)).flatten()
-        results = pd.DataFrame({'true': true_values[self.look_back:]}, index=self.y_valid.iloc[self.look_back:].index)
-        results[self.y_label] = pred.flatten()
-        return results
+    def validation_predict(self, x, y):
+        self.build_model(n_features=x.shape[1])
+        train_size = int(len(x) * 0.8)
+        x_train, x_valid = x.iloc[:train_size], x.iloc[train_size:]
+        y_train, y_valid = y.iloc[:train_size], y.iloc[train_size:]
+        train_gen = TimeseriesGenerator(x_train, y_train, length=self.look_back, batch_size=self.batch_size)
+        valid_gen = TimeseriesGenerator(x_valid, y_valid, length=self.look_back, batch_size=self.batch_size)
+
+        pred = self.model.predict(valid_gen)
+        return pred
 
     def future_predict_preprocess(self, n_periods):
         # generate data set that includes train and validation
@@ -110,21 +91,17 @@ class LSTMForecaster:
 
         return final_train_x, final_train_y, future_exog
 
-    def one_step_future_predict(self):
-        n_periods = utils.num_hours_between_timestamps(self.start_test, self.end_test)
-        final_data = self.data.loc[self.data.index < self.start_test]
+    def predict(self, x):
+        """
+        x is a pandas DataFrame contains the data features - exogenous and lagged target
+        x MUST INCLUDE THE LAST `look_back' PERIODS OF THE TRAIN DATA
+        x.shape = (len(test_data) + look_back, n_features)
+        """
+        x = pd.concat([self.train_tail, x], axis=0)
 
-        # normalize the final data
-        final_data = Preprocess.transform(final_data, columns=self.standard_cols, scalers=self.scalers)
-        final_x = final_data.loc[:, [col for col in final_data.columns if col != self.y_label]]
-
-        # prepare the future periods exogenous data
-        future_exog = self.data.loc[(self.data.index >= self.start_test) & (self.data.index < self.end_test)]
-        future_exog = Preprocess.transform(future_exog, columns=self.standard_cols, scalers=self.scalers)
-        future_exog = future_exog.drop(self.y_label, axis=1)
-        future_exog = future_exog.values
-
-        initial_sequence_x = final_x.values[-self.look_back:]
+        n_periods = len(x) - self.look_back
+        future_exog = x.iloc[self.look_back:].values
+        initial_sequence_x = x.iloc[:self.look_back].values
 
         forecast = []
         current_sequence_x = initial_sequence_x.copy()
@@ -139,68 +116,57 @@ class LSTMForecaster:
             # 2. Append the latest feature data from future_exog
             current_sequence_x = np.vstack((current_sequence_x[1:], future_exog[i].reshape(1, -1)))
 
-        forecast = np.array(forecast).reshape(-1, 1)
-        forecast = self.scalers[self.y_label].inverse_transform(forecast).flatten()
-        forecast_period_dates = pd.date_range(start=self.start_test,
-                                              end=self.end_test - datetime.timedelta(hours=1), freq='1H')
-        forecast = pd.DataFrame({self.y_label: forecast}, index=forecast_period_dates)
         return forecast
 
 
-def predict_all_dmas(data, start_train, start_valid, start_test, end_test, standard_cols):
+def predict_all_dmas(data, start_train, start_test, end_test, cols_to_lag):
     test_true = data.loc[(data.index >= start_test) & (data.index < end_test)]
     df = pd.DataFrame(index=pd.date_range(start=start_test, end=end_test - datetime.timedelta(hours=1), freq='1H'))
 
     fig, axes = plt.subplots(nrows=len(constants.DMA_NAMES), sharex=True, figsize=(12, 9))
 
     for i, dma in enumerate(constants.DMA_NAMES):
-        lstm = LSTMForecaster(data=data, start_train=start_train, start_valid=start_valid, start_test=start_test,
-                              end_test=end_test, y_label=dma, look_back=12, standard_cols=standard_cols)
+        temp_data = data.copy()
+        temp_data = utils.drop_other_dmas(temp_data, dma)
+        temp_data, lagged_cols = preprocess.lag_features(temp_data, cols_to_lag=cols_to_lag)
+        standard_cols = constants.WEATHER_COLUMNS + lagged_cols + [dma]
+        x_train, y_train, x_test, y_test, scalers = Preprocess.split_data(data=temp_data,
+                                                                          y_label=dma,
+                                                                          start_train=start_train,
+                                                                          start_test=start_test,
+                                                                          end_test=end_test,
+                                                                          norm_method='standard',
+                                                                          standard_cols=standard_cols)
 
-        lstm.fit()
-        pred = lstm.one_step_future_predict()
-        df = pd.merge(df, pred, left_index=True, right_index=True)
-        axes[i] = graphs.plot_test(test_true[dma], pred, ax=axes[i])
+        look_back = 24
+        lstm = LSTMForecaster(look_back=look_back, epochs=10, batch_size=24)
+        lstm.fit(x_train, y_train)
+
+        # x_test MUST INCLUDE THE LAST `look_back` PERIODS OF THE TRAIN DATA
+        x_test = pd.concat([x_train.iloc[-look_back:], x_test])
+
+        pred = lstm.predict(x_test)
+        pred = np.array(pred).reshape(-1, 1)
+        pred = scalers[dma].inverse_transform(pred).flatten()
+
+        df[dma] = pred
+        axes[i] = graphs.plot_test(test_true[dma], df[dma], ax=axes[i])
         axes[i].set_ylabel(dma[:5])
 
     fig.align_ylabels()
     fig.subplots_adjust(bottom=0.05, top=0.95, left=0.1, right=0.9, hspace=0.1)
-
-    fig = graphs.plot_time_series(data=test_true, columns=constants.DMA_NAMES)
-    fig = graphs.plot_time_series(data=df, columns=constants.DMA_NAMES, fig=fig)
-
     return df
 
 
 if __name__ == "__main__":
     # usage example
-    y_label = constants.DMA_NAMES[0]
     loader = Loader()
     preprocess = Preprocess(loader.inflow, loader.weather, cyclic_time_features=True, n_neighbors=3)
     data = preprocess.data
-    data, lagged_cols = preprocess.lag_features(data, cols_to_lag={'Air humidity (%)': 6})
 
-    start_train = constants.TZ.localize(datetime.datetime(2022, 1, 1, 0, 0))
-    start_valid = constants.TZ.localize(datetime.datetime(2022, 7, 11, 0, 0))
-    start_test = constants.TZ.localize(datetime.datetime(2022, 7, 18, 0, 0))
+    start_train = constants.DATES_OF_LATEST_WEEK['start_train']
+    start_test = constants.DATES_OF_LATEST_WEEK['start_test']
     end_test = start_test + datetime.timedelta(hours=168)
-
-    predict_all_dmas(data, start_train=start_train, start_valid=start_valid, start_test=start_test, end_test=end_test,
-                     standard_cols=constants.WEATHER_COLUMNS + lagged_cols)
-
-    # lstm = LSTMForecaster(data=data, start_train=start_train, start_valid=start_valid, start_test=start_test,
-    #                       end_test=end_test, y_label=y_label, look_back=6)
-    #
-    # lstm.fit()
-    # valid_pred = lstm.vaildation_predict()
-    # ax = graphs.plot_test(valid_pred['true'], valid_pred[y_label])
-    #
-    # test_pred = lstm.one_step_future_predict()
-    #
-    # # plot test pred on the same plot as train and valid
-    # ax.plot(test_pred)
-    #
-    # # plot test true and test pred
-    # test_true = data.loc[(data.index >= start_test) & (data.index < end_test), y_label]
-    # ax = graphs.plot_test(test_true, pd.DataFrame(test_pred, index=test_pred.index))
+    cols_to_lag = {'Air humidity (%)': 12, 'Rainfall depth (mm)': 12, 'Air temperature (°C)': 12, 'Windspeed (km/h)': 12}
+    predict_all_dmas(data, start_train=start_train, start_test=start_test, end_test=end_test, cols_to_lag=cols_to_lag)
     plt.show()
