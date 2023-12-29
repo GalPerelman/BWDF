6321b6910bd16db94ed8f962b0d9f7a82130a04d
GalPerelman <peligur@gmail.com>
2023-12-29 16:10:57 +0200

    add file to hold clustering options for multi_series

---
+++ clusters.py
@@ -0,0 +1,31 @@
+
+clusters = {
+              0: {
+                    "DMA A (L/s)": ["DMA J (L/s)"],
+                    "DMA B (L/s)": ["DMA C (L/s)"],
+                    "DMA C (L/s)": ["DMA B (L/s)", "DMA E (L/s)", "DMA G (L/s)"],
+                    "DMA D (L/s)": ["DMA E (L/s)", "DMA G (L/s)", "DMA H (L/s)"],
+                    "DMA E (L/s)": ["DMA D (L/s)", "DMA H (L/s)"],
+                    "DMA F (L/s)": ["DMA G (L/s)"],
+                    "DMA G (L/s)": ["DMA D (L/s)", "DMA E (L/s)", "DMA H (L/s)"],
+                    "DMA H (L/s)": ["DMA D (L/s)", "DMA E (L/s)", "DMA G (L/s)", "DMA J (L/s)"],
+                    "DMA I (L/s)": ["DMA J (L/s)"],
+                    "DMA J (L/s)": []
+
+                },
+
+              1: {
+                    "DMA A (L/s)": ["DMA H (L/s)", "DMA J (L/s)"],
+                    "DMA B (L/s)": ["DMA C (L/s)", "DMA E (L/s)"],
+                    "DMA C (L/s)": ["DMA B (L/s)", "DMA D (L/s)", "DMA E (L/s)", "DMA G (L/s)"],
+                    "DMA D (L/s)": ["DMA C (L/s)", "DMA E (L/s)", "DMA G (L/s)", "DMA H (L/s)"],
+                    "DMA E (L/s)": ["DMA D (L/s)", "DMA G (L/s)" ,"DMA H (L/s)"],
+                    "DMA F (L/s)": ["DMA E (L/s)", "DMA G (L/s)"],
+                    "DMA G (L/s)": ["DMA C (L/s)", "DMA D (L/s)", "DMA E (L/s)", "DMA H (L/s)"],
+                    "DMA H (L/s)": ["DMA B (L/s)", "DMA D (L/s)", "DMA E (L/s)", "DMA G (L/s)", "DMA J (L/s)"],
+                    "DMA I (L/s)": ["DMA H (L/s)", "DMA J (L/s)"],
+                    "DMA J (L/s)": ["DMA H (L/s)", "DMA I (L/s)"]
+
+                }
+
+}
