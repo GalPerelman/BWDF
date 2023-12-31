f02398c5b27286c887b13f0e37fd75261613add0
GalPerelman <peligur@gmail.com>
2023-12-31 20:17:34 +0200

    cluster configurations for multi series experiments

--- clusters.py
+++ clusters.py
@@ -1,31 +1,45 @@
-
 clusters = {
-              0: {
-                    "DMA A (L/s)": ["DMA J (L/s)"],
-                    "DMA B (L/s)": ["DMA C (L/s)"],
-                    "DMA C (L/s)": ["DMA B (L/s)", "DMA E (L/s)", "DMA G (L/s)"],
-                    "DMA D (L/s)": ["DMA E (L/s)", "DMA G (L/s)", "DMA H (L/s)"],
-                    "DMA E (L/s)": ["DMA D (L/s)", "DMA H (L/s)"],
-                    "DMA F (L/s)": ["DMA G (L/s)"],
-                    "DMA G (L/s)": ["DMA D (L/s)", "DMA E (L/s)", "DMA H (L/s)"],
-                    "DMA H (L/s)": ["DMA D (L/s)", "DMA E (L/s)", "DMA G (L/s)", "DMA J (L/s)"],
-                    "DMA I (L/s)": ["DMA J (L/s)"],
-                    "DMA J (L/s)": []
+    0: {
+        "DMA A (L/s)": ["DMA J (L/s)"],
+        "DMA B (L/s)": ["DMA C (L/s)"],
+        "DMA C (L/s)": ["DMA B (L/s)", "DMA E (L/s)", "DMA G (L/s)"],
+        "DMA D (L/s)": ["DMA E (L/s)", "DMA G (L/s)", "DMA H (L/s)"],
+        "DMA E (L/s)": ["DMA D (L/s)", "DMA H (L/s)"],
+        "DMA F (L/s)": ["DMA G (L/s)"],
+        "DMA G (L/s)": ["DMA D (L/s)", "DMA E (L/s)", "DMA H (L/s)"],
+        "DMA H (L/s)": ["DMA D (L/s)", "DMA E (L/s)", "DMA G (L/s)", "DMA J (L/s)"],
+        "DMA I (L/s)": ["DMA J (L/s)"],
+        "DMA J (L/s)": []
 
-                },
+    },
 
-              1: {
-                    "DMA A (L/s)": ["DMA H (L/s)", "DMA J (L/s)"],
-                    "DMA B (L/s)": ["DMA C (L/s)", "DMA E (L/s)"],
-                    "DMA C (L/s)": ["DMA B (L/s)", "DMA D (L/s)", "DMA E (L/s)", "DMA G (L/s)"],
-                    "DMA D (L/s)": ["DMA C (L/s)", "DMA E (L/s)", "DMA G (L/s)", "DMA H (L/s)"],
-                    "DMA E (L/s)": ["DMA D (L/s)", "DMA G (L/s)" ,"DMA H (L/s)"],
-                    "DMA F (L/s)": ["DMA E (L/s)", "DMA G (L/s)"],
-                    "DMA G (L/s)": ["DMA C (L/s)", "DMA D (L/s)", "DMA E (L/s)", "DMA H (L/s)"],
-                    "DMA H (L/s)": ["DMA B (L/s)", "DMA D (L/s)", "DMA E (L/s)", "DMA G (L/s)", "DMA J (L/s)"],
-                    "DMA I (L/s)": ["DMA H (L/s)", "DMA J (L/s)"],
-                    "DMA J (L/s)": ["DMA H (L/s)", "DMA I (L/s)"]
+    1: {
+        "DMA A (L/s)": ["DMA H (L/s)", "DMA J (L/s)"],
+        "DMA B (L/s)": ["DMA C (L/s)", "DMA E (L/s)"],
+        "DMA C (L/s)": ["DMA B (L/s)", "DMA D (L/s)", "DMA E (L/s)", "DMA G (L/s)"],
+        "DMA D (L/s)": ["DMA C (L/s)", "DMA E (L/s)", "DMA G (L/s)", "DMA H (L/s)"],
+        "DMA E (L/s)": ["DMA D (L/s)", "DMA G (L/s)", "DMA H (L/s)"],
+        "DMA F (L/s)": ["DMA E (L/s)", "DMA G (L/s)"],
+        "DMA G (L/s)": ["DMA C (L/s)", "DMA D (L/s)", "DMA E (L/s)", "DMA H (L/s)"],
+        "DMA H (L/s)": ["DMA B (L/s)", "DMA D (L/s)", "DMA E (L/s)", "DMA G (L/s)", "DMA J (L/s)"],
+        "DMA I (L/s)": ["DMA H (L/s)", "DMA J (L/s)"],
+        "DMA J (L/s)": ["DMA H (L/s)", "DMA I (L/s)"]
 
-                }
+    },
+
+    2: {
+        "DMA A (L/s)": ["DMA B (L/s)", "DMA C (L/s)", "DMA J (L/s)"],
+        "DMA B (L/s)": ["DMA C (L/s)", "DMA E (L/s)", "DMA G (L/s)"],
+        "DMA C (L/s)": ["DMA B (L/s)", "DMA D (L/s)", "DMA E (L/s)", "DMA G (L/s)"],
+        "DMA D (L/s)": ["DMA B (L/s)", "DMA C (L/s)", "DMA E (L/s)", "DMA G (L/s)", "DMA H (L/s)", "DMA J (L/s)"],
+        "DMA E (L/s)": ["DMA B (L/s)", "DMA C (L/s)", "DMA D (L/s)", "DMA G (L/s)", "DMA H (L/s)", "DMA J (L/s)"],
+        "DMA F (L/s)": ["DMA A (L/s)", "DMA B (L/s)", "DMA C (L/s)", "DMA D (L/s)", "DMA E (L/s)",
+                        "DMA G (L/s)", "DMA H (L/s)", "DMA I (L/s)", "DMA J (L/s)"],
+        "DMA G (L/s)": ["DMA D (L/s)", "DMA E (L/s)", "DMA G (L/s)", "DMA H (L/s)"],
+        "DMA H (L/s)": ["DMA D (L/s)", "DMA E (L/s)", "DMA G (L/s)", "DMA I (L/s)", "DMA J (L/s)"],
+        "DMA I (L/s)": ["DMA E (L/s)", "DMA G (L/s)", "DMA H (L/s)", "DMA J (L/s)"],
+        "DMA J (L/s)": ["DMA D (L/s)", "DMA E (L/s)", "DMA G (L/s)", "DMA H (L/s)", "DMA I (L/s)"]
+
+    }
 
 }
