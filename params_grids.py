2227d5ec2c54dcd0cff01b93b7b0da23fc20a6af
GalPerelman <peligur@gmail.com>
2024-01-07 20:00:56 +0200

    add files totest bootstrap hyperparameter

--- params_grids.py
+++ params_grids.py
@@ -94,6 +94,7 @@ grids = {
             'model': xgb.XGBRegressor(verbosity=0, silent=True),
             'params':
                 {
+                    'bootstrap': [True],
                     "max_depth": [3, 5, 10],
                     "learning_rate": [0.01, 0.05, 0.1],
                     "n_estimators": [50, 100, 200, 500],
