bbb89af37e642ac5ae5e880268373aa379cf979b
GalPerelman <peligur@gmail.com>
2024-02-26 10:38:33 +0200

    cv experiment for w3

--- experiments_scripts/script_run_experiment_cv.sh
+++ experiments_scripts/script_run_experiment_cv.sh
@@ -7,17 +7,17 @@ for a in ${dma[@]}; do
 	for b in ${horizon[@]}; do
       bash ./create_tmp_empty.sh "python ./wrapper.py
                                       --inflow_data_file Inflow_Data.xlsx
-                                      --weather_data_file Weather_Data_2.xlsx
+                                      --weather_data_file Weather_Data_3.xlsx
                                       --do cv
                                       --cv_start_year 2022
-                                      --cv_start_month 10
-                                      --cv_start_day 3
+                                      --cv_start_month 12
+                                      --cv_start_day 26
                                       --search_params 0
                                       --dma_idx $a
                                       --horizon $b
-                                      --outliers_config_path outliers_config_w2.json
-                                      --cv_candidates_path experiments_analysis/candidates_${b}_w2.json
-                                      --output_dir cv_w2"
+                                      --outliers_config_path outliers_config_w3.json
+                                      --cv_candidates_path experiments_analysis/candidates_${b}_w3.json
+                                      --output_dir cv_w3"
 
 	done
 done
